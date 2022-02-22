ae8f91790110634666cdf7c6b3359d78965753c0
osamurakouhei <k.osamura001012@ezweb.ne.jp>
2022-02-22 16:15:45 +0900

    DBのカラムの追加をした

--- database/migrations/2022_02_20_002421_create_posts_table.php
+++ database/migrations/2022_02_20_002421_create_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->string('title');
+            $table->string('image');
             $table->text('content');
             $table->timestamps();
 
