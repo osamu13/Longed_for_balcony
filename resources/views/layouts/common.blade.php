bb069d7876838d9143dab0644f44fe552ed2894c
osamurakouhei <k.osamura001012@ezweb.ne.jp>
2022-02-22 16:16:31 +0900

    インデントの修正をした

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -63,10 +63,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        ログアウト
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    ログアウト
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
