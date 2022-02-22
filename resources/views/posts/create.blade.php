252b1617e73c2c8e28ed45db9a7cf70d625eade2
osamurakouhei <k.osamura001012@ezweb.ne.jp>
2022-02-22 16:17:50 +0900

    formの追加をした

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,30 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div>
-        <h1>新規投稿</h1>
-    </div>
     <div class="mb-4">
-        <a href="{{ route('posts.index') }}" class="btn btn-success">投稿一覧へ</a>
+        <a href="{{ route('posts.index') }}" class="btn btn-success">投稿一覧へ戻る</a>
     </div>
     <div class="row">
         <div class="">
             <div class="card">
-                <div class="card-header bg-danger text-white">自分のバルコニー写真を投稿して、みんなの意見を貰おう</div>
+                <div class="card-header bg-danger text-white h3">家のバルコニー・ベランダ写真を投稿して、みんなの意見を貰おう</div>
     
                 <div class="card-body">
-                    <form action="{{ route('posts.store') }}" method="POST">
+                    <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                         <div class="form-group mb-4">
                             <label>タイトル</label>
-                            <input type="text" class="form-controller w-100 p-2" placeholder="タイトルを入力してください" name="title">
+                            <input type="text" class="form-controller w-100 p-2" placeholder="タイトルを入力してください" name="title" value="{{ old('title') }}">
                             @if ($errors->first('title'))
                             <p class="text-danger">※{{$errors->first('title')}}</p>
                             @endif
                         </div>
+                        <div class="form-group mb-4">
+                            <label>画像</label>
+                            <input type="file" class="form-controller w-100" name="image" value="{{ old('image') }}">
+                            @if ($errors->first('image'))
+                            <p class="text-danger">※{{$errors->first('image')}}</p>
+                            @endif
+                        </div>
                         <div class="form-group">
                             <label>内容</label>
-                            <textarea type="text" rows="8" class="form-controller w-100" placeholder="内容を入力してください" name="content"></textarea>
+                            <textarea type="text" rows="8" class="form-controller w-100" placeholder="内容を入力してください" name="content">{{ old('content') }}</textarea>
                             @if ($errors->first('content'))
                             <p class="text-danger">※{{$errors->first('content')}}</p>
                             @endif
