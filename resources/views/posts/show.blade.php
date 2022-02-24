55e6c7599ed9ab68cbc9a5320afaa305b9c1e194
osamurakouhei <k.osamura001012@ezweb.ne.jp>
2022-02-24 15:57:54 +0900

    コメントの削除ボタンを追加した

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,23 +3,23 @@
 @section('content')
 <div class="container">
     <div class="mb-4">
-        <a href="{{route('posts.index')}}" class="btn btn-primary">投稿一覧へ戻る</a>
+        <a href="{{route('posts.index')}}" class="btn return-posts">投稿一覧に戻る</a>
     </div>
     <div class="card">
         <div class="card-header bg-success text-white h2">このバルコニー・ベランダには何が合う？</div>
 
-        <div class="card-body">
+        <div class="card-body card-body-bg">
             <h4 class="card-title">{{ $post->title }}</h4>
-            <div class="d-flex">
-                <p class="card-text">投稿者：{{ $post->user->name }}</p>
-                <p class="card-text mx-md-3">投稿日：{{ $post->created_at }}</p>
-                <p class="card-text mx-md-3">更新日：{{ $post->updated_at }}</p>
+            <div class="d-md-flex mb-3">
+                <p class="card-text mb-0">投稿者：{{ $post->user->name }}</p>
+                <p class="card-text mb-0 mx-md-3">投稿日：{{ $post->created_at }}</p>
+                <p class="card-text mb-0 mx-md-3">更新日：{{ $post->updated_at }}</p>
             </div>
             <div class="d-lg-flex justify-content-center">
                 <div class="mb-4 col-lg-8">
                     <img src="{{ '/storage/'.$post->image }}" alt="" class="post_img">
                 </div>
-                <div class="col-lg-4">
+                <div class="col-lg-4 mb-4">
                     <h4>内容</h4>
                     <h5 class="card-text content">{{ $post->content }}</h5>
                 </div>
@@ -55,10 +55,17 @@
     <div class="mt-4">
         @foreach ($comments as $comment)
         <div class="card mb-3">
-            <div class="card-header h5">投稿者：{{ $comment->user->name }}</div>
-            <div class="card-body">
+            <div class="card-header comment-header-bg h5">投稿者：{{ $comment->user->name }}</div>
+            <div class="card-body card-body-bg">
                 <p class="card-title mb-4">投稿日：{{ $comment->created_at }}</p>
                 <h5 class="card-text">{{ $comment->content }}</h5>
+                @if (Auth::id() === $comment->user_id)
+                <form action="{{ route('comments.destroy', $comment->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="削除する" class="btn btn-danger" onclick="return confirm('本当に削除しますか？')">
+                </form>
+                @endif
             </div>
         </div>
         @endforeach
